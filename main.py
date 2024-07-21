b526acde3ca9a7dba5b24db5d3997591975c2349
Alpo van der Knaap <alpo.knaap@gmail.com>
2024-07-21 20:49:08 +0300

    basic version of time pinting using ASCII functional, final commit before trying to implement async functions for weather

--- main.py
+++ main.py
@@ -3,43 +3,103 @@
 import os
 from datetime import datetime
 import time
+import asyncio
+import python_weather
 
-def fullScreenRenderer(time: datetime, weather, location: str):
-  width = os.get_terminal_size().columns
-  height = os.get_terminal_size().lines
-  widthArr = []
-  for i in range(width):
-    widthArr.append('-')
-  widthArr = ''.join(str(i) for i in widthArr)
 
-  #screen construction:
-  print(str(widthArr))
-  for i in range(height - 3):
-    print(time)
-  print(str(widthArr))
+def screenRenderer(timeNow: datetime, weather, location: str):
+    width = os.get_terminal_size().columns
+    height = os.get_terminal_size().lines - 3
 
-  return None
+    widthArr = []
+    for i in range(width):
+      widthArr.append('-')
+    widthArr = ''.join(str(i) for i in widthArr)
+    
+    clockWidth = 8 + 8 + 5 + 8 + 8
+    fillerLen = width - clockWidth - width//6
+    widthFiller = []
+    for i in range(fillerLen):
+      widthFiller.append(' ')
+    widthFiller = ''.join(str(i) for i in widthFiller)
 
-def getWeather():
+    if width < 3*clockWidth//2:
+      print(timeNow.date(), timeNow.time())
+      print('Can not properly display information due to small terminal!')
+      return None
 
-  return None
+    tStr = timeSymbols(timeNow)
+
+    hour1 = getSymbol('numbers.txt', tStr[0])
+    hour2 = getSymbol('numbers.txt', tStr[1])
+    min1 = getSymbol('numbers.txt', tStr[2])
+    min2 = getSymbol('numbers.txt', tStr[3])
+
+    #symLines = [getSymbol('numbers.txt', '0')[0], getSymbol('numbers.txt', '0')[1], getSymbol('numbers.txt', '0')[2], getSymbol('numbers.txt', '0')[3], getSymbol('numbers.txt', '0')[4]]
+
+    #screen construction:
+    print(str(widthArr))
+    print('')
+
+    for i in range(5):
+      print('%s %-8s %-8s %-5s %-8s %-8s' % (widthFiller, hour1[i], hour2[i], getSymbol('numbers.txt', ':')[i], min1[i], min2[i]))
+    print('%s  %s' % (widthFiller, str(timeNow.date())))
+    print('%s  Weather in %s: %s' % (widthFiller, location, str(weather)))
+
+    for i in range((height - 8)):
+      print('')
+    
+    print(str(widthArr))
+    return None
+
+
+def timeSymbols(timeNow: datetime):
+    hours = str(timeNow.hour)
+    minutes = str(timeNow.minute)
+    if len(hours) == 1:
+      hours = '0' + hours
+    if len(minutes) == 1:
+      minutes = '0' + minutes
+    return [hours[0], hours[1], minutes[0], minutes[1]]
+
+
+def getSymbol(fileName, symbol):
+    font = open(fileName, 'r')
+    bigSym = []
+    symStart = False
+    height = 5
+    for lines in font.readlines():
+      if symStart == True and height != 0:
+        bigSym.append(lines.replace('\n', ''))
+        height -= 1
+      elif '[' + symbol + ']' in lines:
+        symStart = True
+    font.close()
+    return bigSym
+
+'''
+async def getWeather(location):
+  async with python_weather.Client(unit=python_weather.METRIC) as client:
+    weather = await client.get(location)
+  return weather.temperature
+'''
 
 def main():
-  #longSeq = [None, None, None, None, None, None, None, None, None, None]  #instructions for 6min steps in 1h loop
-  #shortSeq = [None, None, None, None, None, None, None, None, None, None] #instructions for 6s steps in 1min loop
-  location = 'Helsinki'
+    location = 'Helsinki'
 
-  runnig = True
+    runnig = True
 
-  shortTime = time.time()
-  longTime = time.time()
-  while runnig:
-    if time.time() > 6 + shortTime:
-      fullScreenRenderer(datetime.now(), None, None)
-      shortTime = time.time()
-    if time.time() > 1200 + longTime:
-      getWeather()
-    time.sleep(1)
-  return None
+    #asyncio.run(getWeather(location))
+
+    #weather = getWeather(location)
+    weather = 'something'
+
+    checkTime = time.time()
+    while runnig:
+      screenRenderer(datetime.now(), weather, location)
+      if time.time() > 1200 + checkTime:            #only run this every 20min
+        weather = getWeather(location)
+      time.sleep(20)
+    return None
 
 main()
